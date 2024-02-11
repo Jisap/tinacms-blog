d598ef228991207f65b8944d3b31fdc2062208a0
Jorge Matos <jisapjisap@gmail.com>
2024-02-11 15:09:10 +0100

    configuration

--- tina/config.ts
+++ tina/config.ts
@@ -98,7 +98,7 @@ export default defineConfig({
           filename: {
             slugify: (values) => {
               return `${(values.title || "")}`
-              .toLocaleLowerCase().replace(/ /g, "-")
+              .toLowerCase().replace(/ /g, "-")
               .replace(/[^\w\.\/-\s]/gi, "")
             }
           }
