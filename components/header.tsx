b84c95755aae5cea19e28463265ba19b20d5bf79
Jorge Matos <jisapjisap@gmail.com>
2024-02-14 19:32:24 +0100

    socials

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,36 @@
 import Link from "next/link";
 import { ThemeToggle } from "./ThemeToggle";
+import { RxBookmark, RxChatBubble, RxDividerVertical, RxEnter, RxHome, RxPerson } from "react-icons/rx";
+import { RiFacebookFill, RiGithubFill, RiTwitterFill, RiYoutubeFill } from "react-icons/ri";
 
 export default function Header() {
   return (
     <header className="mb-20 mt-16 flex content-center items-center justify-between font-sans text base">
       <div className="flex gap-4">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/posts">Blog</Link>
+        <Link href="/" className="max-sm:hidden">Home</Link>
+        <Link href="/" className="sm:hidden">
+          <RxHome />
+        </Link>
+        <Link href="/posts" className="max-sm:hidden">Blog</Link>
+        <Link href="/post" className="sm:hidden">
+          <RxChatBubble />
+        </Link>
+        <Link href="/about" className="max-sm:hidden">About</Link>
+        <Link href="/about" className="sm:hidden">
+          <RxPerson />
+        </Link>
       </div>
       <div className="flex justify-between gap-4">
+        <Link href="https://youtube.com">
+          <RiYoutubeFill />
+        </Link>
+        <Link href="https://twitter.com">
+          <RiTwitterFill />
+        </Link>
+        <Link href="https://github.com">
+          <RiGithubFill />
+        </Link>
+        <RxDividerVertical  className="text-gray-300 dark:text-gray-600"/>
         <ThemeToggle />
       </div>
     </header>  
