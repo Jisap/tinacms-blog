8c6cf3b36d53be850dda14588deab7bcf6e32160
Jorge Matos <jisapjisap@gmail.com>
2024-02-19 20:14:08 +0100

    sort post

--- components/app/posts/post-list-page.tsx
+++ components/app/posts/post-list-page.tsx
@@ -17,18 +17,26 @@ export function PostListPageComponent(props: {
   const postList = data.postConnection.edges; // postConnection contiene información sobre la paginación de las publicaciones
                                               // edge es un array cuyos items representan una publicación individual con información específica sobre ella.
 
+  postList?.sort((a:any, b:any) => {
+    const dateA: any=new Date(a.node.date)    // Se crea una nueva instancia de Date apartir de las fecha de los posts adayacentes en el array
+    const dateB: any=new Date(b.node.date)
+    return dateB - dateA  // Si la resta es - B es anterior a A, B debe estar antes en la ordenación. Proceso contrario si la resta es +
+  })
+
   console.log(postList)
   return (
     <>
       <h1>Blog</h1>
       <div>
-        {postList?.map((post:any) => ( 
-          <div key={post.node.id}>
-            <Link href={`/posts/${post.node._sys.filename}`}>
-              {post.node.title}
-            </Link>
-          </div>
-        ))}
+        <ul className="m-0 pl-0">
+          {postList?.map((post:any) => ( 
+            <div key={post.node.id}>
+              <Link href={`/posts/${post.node._sys.filename}`}>
+                {post.node.title}
+              </Link>
+            </div>
+          ))}
+        </ul>
       </div>
     </>
   )
